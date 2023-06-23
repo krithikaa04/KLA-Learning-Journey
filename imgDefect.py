4c8f0f7644c9777dc52773d5b7904f1c9d0bf558
Krithika Lakshman <105552433+krithikaa04@users.noreply.github.com>
2023-06-23 10:58:51 +0530

    Add files via upload

---
+++ imgDefect.py
@@ -0,0 +1,33 @@
+from PIL import Image
+
+def find_defect_areas(wafer1_gray,wafer2_gray,threshold):
+    defective_pixels = []
+
+    # Iterate over each pixel
+    for x in range(wafer1_gray.width):
+        for y in range(wafer1_gray.height):
+            pixel1 = wafer1_gray.getpixel((x, y))
+            pixel2 = wafer2_gray.getpixel((x, y))
+
+            # Compare the pixel values
+            if abs(pixel1 - pixel2) > threshold:
+                defective_pixels.append((x, y))
+
+    return defective_pixels
+
+threshold = 50  # Adjust the threshold value based on your requirements
+wafer1 = Image.open("C:\\Users\\Krithika\Desktop\\PSG\KL workshop- learning journey\\Level_1_Input_Data\\wafer_image_1.png")
+wafer2 = Image.open("C:\\Users\\Krithika\Desktop\\PSG\KL workshop- learning journey\\Level_1_Input_Data\\wafer_image_2.png")
+wafer3 = Image.open("C:\\Users\\Krithika\Desktop\\PSG\KL workshop- learning journey\\Level_1_Input_Data\\wafer_image_3.png")
+
+#convert to gray scale
+wafer1_gray = wafer1.convert("L")
+wafer2_gray = wafer2.convert("L")
+wafer3_gray = wafer3.convert("L")   #consider as reference image
+
+defective_pixels_13 = find_defect_areas(wafer1_gray, wafer3_gray, threshold)
+defective_pixels_23 = find_defect_areas(wafer2_gray, wafer3_gray, threshold)
+
+print(defective_pixels_13)
+print("---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------")
+print(defective_pixels_23)
---
+++ trial1.py
@@ -0,0 +1,39 @@
+import json
+from PIL import Image
+import cv2
+
+
+json_file_path = 'C:\\Users\\Krithika\\Desktop\\PSG\\KL workshop- learning journey\\Level_1_Input_Data\\input.json'
+with open(json_file_path, 'r') as json_file:
+    data = json.load(json_file)
+
+def dict_to_list(nested_dict):
+    if isinstance(nested_dict, dict):
+        return [dict_to_list(value) for value in nested_dict.values()]
+    elif isinstance(nested_dict, list):
+        return [dict_to_list(item) for item in nested_dict]
+    else:
+        return nested_dict
+print(data)
+care_areas = data['care_areas']
+die_position = data['die']
+print(die_position)
+die_width = (die_position.get('width'))
+die_height = (die_position.get('height'))
+num_rows = (die_position.get('rows'))
+num_col = (die_position.get('columns'))
+print(die_height)
+print(care_areas)
+
+for i in care_areas:
+    #nested_dict = i
+    care_areas_coord = dict_to_list(i)
+    print(care_areas_coord)
+#Extract the pixel data within the die region from the wafer image.
+img1 = cv2.imread('C:\Users\Krithika\Desktop\PSG\KL workshop- learning journey\Level_1_Input_Data\wafer_image_1.png', 0)  
+img2 = cv2.imread('C:\Users\Krithika\Desktop\PSG\KL workshop- learning journey\Level_1_Input_Data\wafer_image_2.png', 0)  
+img3 = cv2.imread('C:\Users\Krithika\Desktop\PSG\KL workshop- learning journey\Level_1_Input_Data\wafer_image_3.png', 0)  
+img4 = cv2.imread('C:\Users\Krithika\Desktop\PSG\KL workshop- learning journey\Level_1_Input_Data\wafer_image_4.png', 0)  
+img5 = cv2.imread('C:\Users\Krithika\Desktop\PSG\KL workshop- learning journey\Level_1_Input_Data\wafer_image_5.png', 0)  
+ 
+
